6221dac6d690c1f64edfcc505615a040a3ee3d4f
Nourreddine1920 <nourreddine2000@gmail.com>
2021-02-21 12:52:19 +0100

    group 9 _ hackaton CLL

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ git init
 git add .
 git status
 git config --global user.name "Nourreddine1920"
-git config --global user.password "nouri202"
+git config --global user.password "nour202"
 git config --global user.email "nourreddine2000@gmail.com"
 git commit -m "group 9 _ hackaton CLL"
 git remote add origin https://github.com/Nourreddine1920/hackaton-CLL.git
